c917df1265a4cc774980e706d087f2552a62d876
Raian-Siddique <raian0197@gmail.com>
2022-11-12 01:55:50 +0100

    final1

---
+++ butterflyPattern.java
@@ -0,0 +1,44 @@
+/**
+ * butterflyPattern
+ */
+
+public class butterflyPattern {
+
+    public static void butterfly(int n) {
+
+        // Outer loop || First Half
+        for (int i = 1; i <= n; i++) {
+            // Inner Loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Second Half|| inverted
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        butterfly(5);
+    }
+}
